92ddd4976e9bc56c0c3ecfc17ee79a713fb950dd
Vineet KUMAR <vkumar5@cougar.(none)>
2013-02-04 16:02:04 -0500

    updated jastadd code
    Migrated from Sable/mclab@aa75df3e169eb8c627ba833001c260e7c80d92ae

--- languages/Natlab/src/natlab/backends/x10/Main.java
+++ languages/Natlab/src/natlab/backends/x10/Main.java
@@ -21,7 +21,7 @@ public class Main {
 		// String file =
 		// "/home/2011/vkumar5/mclab/Project/languages/Natlab/src/natlab/backends/x10/testing/unitTests/ut8"
 		// ;
-		String file = "/home/2011/vkumar5/mclab_git/mclab/languages/Natlab/src/natlab/backends/x10/benchmarks/unit/forTest1_tame";
+		String file = "/home/2011/vkumar5/mclab_git/mclab/languages/Natlab/src/natlab/backends/x10/benchmarks/unit/forTest1";
 
 		//String file = "/home/2011/vkumar5/hello1";
 		String fileIn = file + ".m";
